fb806a9932a9c7b4b74b933d5c65b21466880253
DarkZangetsu <113225391+DarkZangetsu@users.noreply.github.com>
2025-02-10 08:42:46 +0300

    update primary button color

--- src/components/button/primaryButton.jsx
+++ src/components/button/primaryButton.jsx
@@ -1,10 +1,11 @@
 "use client";
+
 export default function primaryButton({label, onClick}) {
   return (
     <div>
       <button 
         onClick={onClick}
-        className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+        className="bg-blue-700 hover:bg-blue-600 text-white font-bold py-2 px-4 border-b-4 border-blue-900 hover:border-blue-700 rounded"
         >
         {label}
       </button>
